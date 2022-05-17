305d00c720a138ed69ae77d41bdb0385ebd9332d
Amit Kumar <amit.kumars2175@gmail.com>
2022-05-17 17:18:06 +0530

    Diameter

--- src/main/java/DiameterBinaryTree.java
+++ src/main/java/DiameterBinaryTree.java
@@ -1,5 +1,6 @@
 public class DiameterBinaryTree {
-    
+
+    static int ans =0;
    public static void main(String args[]){
       /*
                 2
@@ -9,30 +10,41 @@ public class DiameterBinaryTree {
        10
        
       */
-      Node root  = new Node(2);
-      root.left = new Node(3);
-      root.right = new Node(4);
-      root.left.left = new Node(5);
-      root.left.right = new Node(6);
-      root.right.left = new Node(7);
-      root.right.right = new Node(8);
-      root.left.left.left = new Node(9);
-      root.left.left.left.left = new Node(10);
-      assert(findDiameter(root)==7);
+      TreeNode root  = new TreeNode(2);
+      root.left = new TreeNode(3);
+      root.right = new TreeNode(4);
+      root.left.left = new TreeNode(5);
+      root.left.right = new TreeNode(6);
+      root.right.left = new TreeNode(7);
+      root.right.right = new TreeNode(8);
+      root.left.left.left = new TreeNode(9);
+      root.left.left.left.left = new TreeNode(10);
+      System.out.println(findDiameter(root));
     }
-   public static findDiameter(Node root) {
-          if(root ==  null) return 0;
-           dLeft = findDiameter(root.left);
-           dRight = findDiameter(root.right);
-          return Math.max(dLeft,dRight, 1+ dLeft+dRight);
+   public static int findDiameter(TreeNode root) {
+//          if(root ==  null) return 0;
+//           int dLeft = findDiameter(root.left);
+//           int dRight = findDiameter(root.right);
+//           int lHeight = height(root.left);
+//           int rHeight = height(root.right);
+//          return Math.max(Math.max(dLeft,dRight), 1+ lHeight+rHeight);
+           height(root);
+           return ans;
+   }
+   private static int  height(TreeNode root) {
+       if(root ==  null ) return 0;
+       int lHeight = height(root.left);
+       int rHeight = height(root.right);
+       ans = Math.max(ans,1+ lHeight+rHeight);
+       return Math.max(1+lHeight,1+rHeight);
    }
 }
-class Node{
-  Node left;
-  Node right;
+class TreeNode {
+  TreeNode left;
+  TreeNode right;
   int data;
-  
-  Node(int data){
+
+  TreeNode(int data){
     this.data = data;
     left=right=null;
   }
